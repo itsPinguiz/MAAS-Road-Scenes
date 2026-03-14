4f26eba0c2a28934e7ba4799f7ab9a34d398e5b8
Stefano Zizzi <zizzi.stfn@gmail.com>
2026-03-14 11:08:09 +0100

    edited eval miou

--- eval/eval_iou.py
+++ eval/eval_iou.py
@@ -1,32 +1,18 @@
-# Code to calculate IoU (mean and per-class) in a dataset
-# Nov 2017
-# Eduardo Romera
-#######################
-
-import numpy as np
+import os
 import torch
 import torch.nn.functional as F
-import os
-import importlib
-import time
-
 from PIL import Image
 from argparse import ArgumentParser
+from torchvision.transforms import Compose, Resize, ToTensor
+from tqdm import tqdm
 
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torchvision.transforms import Compose, CenterCrop, Normalize, Resize
-from torchvision.transforms import ToTensor, ToPILImage
-
-from dataset import cityscapes
 from erfnet import ERFNet
-from transform import Relabel, ToLabel, Colorize
+from transform import Relabel, ToLabel
 from iouEval import iouEval, getColorEntry
 
-NUM_CHANNELS = 3
 NUM_CLASSES = 20
+IGNORE_INDEX = 19
 
-image_transform = ToPILImage()
 input_transform_cityscapes = Compose([
     Resize(512, Image.BILINEAR),
     ToTensor(),
@@ -34,116 +20,89 @@ input_transform_cityscapes = Compose([
 target_transform_cityscapes = Compose([
     Resize(512, Image.NEAREST),
     ToLabel(),
-    Relabel(255, 19),   #ignore label to 19
+    Relabel(255, IGNORE_INDEX),   # ignore label to 19
 ])
 
+def load_my_state_dict(model, state_dict):
+    own_state = model.state_dict()
+    for name, param in state_dict.items():
+        if name not in own_state:
+            if name.startswith("module."):
+                own_state[name.split("module.")[-1]].copy_(param)
+            else:
+                continue
+        else:
+            own_state[name].copy_(param)
+    return model
+
 def main(args):
+    modelpath = os.path.join(args.loadDir, args.loadModel)
+    weightspath = os.path.join(args.loadDir, args.loadWeights)
 
-    modelpath = args.loadDir + args.loadModel
-    weightspath = args.loadDir + args.loadWeights
-
-    print ("Loading model: " + modelpath)
-    print ("Loading weights: " + weightspath)
+    print("Loading model: " + modelpath)
+    print("Loading weights: " + weightspath)
 
     model = ERFNet(NUM_CLASSES)
 
-    #model = torch.nn.DataParallel(model)
-    if (not args.cpu):
+    if not args.cpu:
         model = torch.nn.DataParallel(model).cuda()
 
-    def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
-        own_state = model.state_dict()
-        for name, param in state_dict.items():
-            if name not in own_state:
-                if name.startswith("module."):
-                    own_state[name.split("module.")[-1]].copy_(param)
-                else:
-                    print(name, " not loaded")
-                    continue
-            else:
-                own_state[name].copy_(param)
-        return model
-
     model = load_my_state_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
-    print ("Model and weights LOADED successfully")
-
-
+    print("Model and weights LOADED successfully")
     model.eval()
 
-    if(not os.path.exists(args.datadir)):
-        print ("Error: datadir could not be loaded")
+    # Ricerca infallibile con os.walk (come fatto in EoMT)
+    image_paths = []
+    for root, dirs, files in os.walk(args.datadir):
+        if 'val' in root.split(os.sep): 
+            for file in files:
+                if file.endswith("leftImg8bit.png"):
+                    image_paths.append(os.path.join(root, file))
 
+    print(f"DEBUG: Trovate {len(image_paths)} immagini 'leftImg8bit.png' nella cartella 'val'.")
 
-    loader = DataLoader(cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset), num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
+    if len(image_paths) == 0:
+        print(f"ERRORE CRITICO: Nessuna immagine trovata in {args.datadir}")
+        return
 
+    iouEvalVal = iouEval(NUM_CLASSES, ignoreIndex=IGNORE_INDEX)
 
-    iouEvalVal = iouEval(NUM_CLASSES)
+    for img_path in tqdm(image_paths, desc="Evaluating ERFNet"):
+        # Ricostruzione sicura del path della ground truth
+        gt_path = img_path.replace('leftImg8bit_trainvaltest', 'gtFine_trainvaltest') \
+                          .replace('leftImg8bit', 'gtFine') \
+                          .replace('.png', '_labelIds.png')
+        
+        if not os.path.exists(gt_path):
+            print(f"ATTENZIONE: Manca la label per {img_path}")
+            continue
 
-    start = time.time()
+        img = Image.open(img_path).convert('RGB')
+        gt = Image.open(gt_path)
 
-    for step, (images, labels, filename, filenameGt) in enumerate(loader):
-        if (not args.cpu):
-            images = images.cuda()
-            labels = labels.cuda()
+        if not args.cpu:
+            img_t = input_transform_cityscapes(img).unsqueeze(0).cuda()
+            gt_t = target_transform_cityscapes(gt).unsqueeze(0).cuda()
+        else:
+            img_t = input_transform_cityscapes(img).unsqueeze(0)
+            gt_t = target_transform_cityscapes(gt).unsqueeze(0)
 
-        inputs = Variable(images)
         with torch.no_grad():
-            outputs = model(inputs)
+            outputs = model(img_t)
 
-        iouEvalVal.addBatch(outputs.max(1)[1].unsqueeze(1).data, labels)
-
-        filenameSave = filename[0].split("leftImg8bit/")[1] 
-
-        print (step, filenameSave)
-
+        iouEvalVal.addBatch(outputs.max(1)[1].unsqueeze(1).data, gt_t)
 
     iouVal, iou_classes = iouEvalVal.getIoU()
 
-    iou_classes_str = []
-    for i in range(iou_classes.size(0)):
-        iouStr = getColorEntry(iou_classes[i])+'{:0.2f}'.format(iou_classes[i]*100) + '\033[0m'
-        iou_classes_str.append(iouStr)
-
-    print("---------------------------------------")
-    print("Took ", time.time()-start, "seconds")
     print("=======================================")
-    #print("TOTAL IOU: ", iou * 100, "%")
-    print("Per-Class IoU:")
-    print(iou_classes_str[0], "Road")
-    print(iou_classes_str[1], "sidewalk")
-    print(iou_classes_str[2], "building")
-    print(iou_classes_str[3], "wall")
-    print(iou_classes_str[4], "fence")
-    print(iou_classes_str[5], "pole")
-    print(iou_classes_str[6], "traffic light")
-    print(iou_classes_str[7], "traffic sign")
-    print(iou_classes_str[8], "vegetation")
-    print(iou_classes_str[9], "terrain")
-    print(iou_classes_str[10], "sky")
-    print(iou_classes_str[11], "person")
-    print(iou_classes_str[12], "rider")
-    print(iou_classes_str[13], "car")
-    print(iou_classes_str[14], "truck")
-    print(iou_classes_str[15], "bus")
-    print(iou_classes_str[16], "train")
-    print(iou_classes_str[17], "motorcycle")
-    print(iou_classes_str[18], "bicycle")
-    print("=======================================")
-    iouStr = getColorEntry(iouVal)+'{:0.2f}'.format(iouVal*100) + '\033[0m'
-    print ("MEAN IoU: ", iouStr, "%")
+    print(f"ERFNet_mIoU_FINAL: {iouVal.item() * 100:.2f}%")
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-
-    parser.add_argument('--state')
-
-    parser.add_argument('--loadDir',default="../trained_models/")
+    parser.add_argument('--loadDir', default="../trained_models/")
     parser.add_argument('--loadWeights', default="erfnet_pretrained.pth")
     parser.add_argument('--loadModel', default="erfnet.py")
-    parser.add_argument('--subset', default="val")  #can be val or train (must have labels)
-    parser.add_argument('--datadir', default="/home/shyam/ViT-Adapter/segmentation/data/cityscapes/")
-    parser.add_argument('--num-workers', type=int, default=4)
-    parser.add_argument('--batch-size', type=int, default=1)
+    parser.add_argument('--datadir', default="../Datasets/Cityscapes")
     parser.add_argument('--cpu', action='store_true')
 
     main(parser.parse_args())
